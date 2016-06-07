f7b28fbecc65c918779d8258972d3fe30a77ca2f
Sara Safavi <sara@sarasafavi.com>
2016-06-07 11:03:48 -0500

    add 'sites' dependency to initial migration

--- account/migrations/0001_initial.py
+++ account/migrations/0001_initial.py
@@ -32,6 +32,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('sites', '0001_initial')
     ]
 
     operations = [
